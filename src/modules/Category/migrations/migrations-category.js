259a5750726b2e641e4cbffdfbeab27b8965da41
Cuonggkw <cp9188950@gmail.com>
2024-10-30 17:10:00 +0700

    api

---
+++ src/modules/Category/migrations/migrations-category.js
@@ -0,0 +1,36 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("categories", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      categoryName: {
+        type: Sequelize.STRING,
+      },
+      parent_category_ID: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+      },
+      status: {
+        type: Sequelize.INTEGER,
+        defaultValue: 0,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("categories");
+  },
+};
---
+++ src/modules/Category/models/category.js
@@ -0,0 +1,26 @@
+import { DataTypes, Model } from "sequelize";
+import sequelize from "../../../core/database";
+
+class Categories extends Model {
+  static associate(models) {}
+}
+
+Categories.init(
+  {
+    categoryName: DataTypes.STRING,
+    parent_category_ID: DataTypes.INTEGER,
+    status: DataTypes.INTEGER,
+  },
+  {
+    sequelize,
+    modelName: "Categories",
+  }
+);
+
+Categories.belongsTo(Categories, {
+  as: "parentCategory",
+  foreignKey: "parent_category_ID",
+  targetKey:"id",
+});
+
+module.exports = Categories;
